fd8dbacf6884bb99777b5d3914018fa3d0ec0a40
LeonardoMorenoV <leonardommorenov@gmail.com>
2015-09-11 09:47:10 -0500

    Initial commit

---
+++ app/src/androidTest/java/samael/mx/edu/tese/landscapeintent6801/ApplicationTest.java
@@ -0,0 +1,13 @@
+package samael.mx.edu.tese.landscapeintent6801;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
